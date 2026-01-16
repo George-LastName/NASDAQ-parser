c6a67aaa8fc68b7d14a66c0a69a9c4da5238537a
GeeBow <gfbowron@gmail.com>
2026-01-16 00:41:13 +0000

    Begun decoding efforts.
    Decided to keep it simple for now and just decode. UPD stuff later :)

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,51 @@
+#include <cstdint>
 #include <cstdlib>
 #include <fcntl.h>
 #include <unistd.h>
 #include <iostream>
-#include <iomanip>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <span>
+
+
+void parse_message(std::uint8_t* filePtr, off_t* offset){
+    // Decode Common Message Start
+    // Name: Offset, Length.
+
+    // Message Type: 0, 1
+    std::cout << std::dec;
+    char type = static_cast<char>(filePtr[*offset]);
+    std::cout << type << " | ";
+
+    // Stock Locate: 1, 2
+    int stk_loc = (static_cast<uint16_t>(filePtr[*offset+1]) << 8) | filePtr[*offset+2];
+    std::cout << stk_loc << " | ";
+
+    // Tracking Num: 3, 2
+    int trk_num = (static_cast<uint16_t>(filePtr[*offset+3]) << 8) | filePtr[*offset+4];
+    std::cout << trk_num << " | ";
+
+    // Timestamp: 5, 6
+    size_t tsmp_offs = 5;
+    size_t tsmp_size = 6;
+    std::uint64_t timestamp = 0;
+    auto time_span = std::span(filePtr + *offset+tsmp_offs, tsmp_size);
+
+    for(std::uint8_t t :time_span){
+        timestamp = (timestamp << 8) | t;
+    }
+
+    std::cout << timestamp;
+    std::cout << " || ";
+    switch(type){
+        case 'S' : std::cout << "Ssss"; break;
+        case 'R' : std::cout << "Rrrr"; break;
+        default: std::cout << "Unknown Message Type\n"; break;
+    }
+    std::cout << " ¬";
+};
 
 int main(int argc, char* argv[]){
-    int rdonly_file, message_l;
-    off_t file_size;
-    struct stat file_stats;
 
     if (argc != 2) {
         std::cout << "Second argument must be file path/filename." << std::endl;
@@ -18,19 +54,20 @@ int main(int argc, char* argv[]){
 
     const char* filepath = argv[1];
 
-    rdonly_file = open(filepath, O_RDONLY);
+    int rdonly_file = open(filepath, O_RDONLY);
     if (rdonly_file == -1){
         std::cout << "Failed to open file" << std::endl;
         exit(EXIT_FAILURE);
     }
 
+    struct stat file_stats;
     if(fstat(rdonly_file, &file_stats) == -1){
         std::cout << "Failed to get file size." << std::endl;
         close(rdonly_file);
         exit(EXIT_FAILURE);
     }
 
-    file_size = file_stats.st_size;
+    off_t file_size = file_stats.st_size;
     std::cout << "Size of selected file (bytes): " << file_size << std::endl;
 
     std::uint8_t* mapped_file = static_cast<std::uint8_t*>(mmap((caddr_t)0, file_size, PROT_READ, MAP_SHARED, rdonly_file, 0));
@@ -41,38 +78,25 @@ int main(int argc, char* argv[]){
     }
     close(rdonly_file);
 
-    std::cout << std::hex;
-    for(off_t i = 0; i < 16; i++){
-        //std::cout << std::setw(2) << std::setfill('0') << static_cast<int>(mapped_file[i]) << " ";
-        std::cout << static_cast<int>(mapped_file[i]) << " ";
-        //std::cout << mapped_file[i] << " ";
-    }
-    std::cout << std::endl;
-
-    for(off_t j = 15; j >= 0; j--){
-        //std::cout << std::setw(2) << std::setfill('0') << static_cast<int>(mapped_file[file_size-1-j]) << " ";
-        std::cout << static_cast<int>(mapped_file[file_size-1-j]) << " ";
-        //std::cout << mapped_file[file_size-1-j] << " ";
-    }
-    std::cout << std::endl;
-    std::cout << std::dec;
-
     off_t offset = 0;
+    uint16_t message_l;
     std::cout <<file_size << std::endl;
-    while (offset < file_size) {
-    //for(int num = 0; num < 100; num++){
-        std::cout << std::dec;0
-        message_l = static_cast<int>(mapped_file[offset]) + static_cast<int>(mapped_file[offset+1]);
-        std::cout <<message_l;// << ", " << offset;
+    //while (offset < file_size) {
+    for(int num = 0; num < 2; num++){
+        std::cout << std::dec;
+        message_l = (static_cast<uint16_t>(mapped_file[offset]) << 8) | mapped_file[offset+1];
+        std::cout << message_l;
         std::cout << " | ";
 
         std::cout << std::hex;
-        std::cout << std::setw(2) << std::setfill('0') << static_cast<int>(mapped_file[offset]) << " ";
-        std::cout << std::setw(2) << std::setfill('0') << static_cast<int>(mapped_file[offset+1]) << " ";
+        std::cout << static_cast<uint16_t>(mapped_file[offset]) << " ";
+        std::cout << static_cast<uint16_t>(mapped_file[offset+1]) << " ";
         offset += 2;
         for(off_t m = 0; m < message_l; m++){
-            std::cout << std::setw(2) << std::setfill('0') << static_cast<int>(mapped_file[offset+m]) << " ";
+            std::cout << static_cast<uint16_t>(mapped_file[offset+m]) << " ";
         }
+        std::cout << "\n   | _ __ ";
+        parse_message(mapped_file, &offset);
         std::cout << std::endl;
         offset += message_l;
     }
